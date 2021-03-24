93f462807cfcd85753f8416edb277e9dcf87d6e5
Guido1956 <GuidoDulos@valori.nl>
2021-03-24 08:20:53 +0100

    Versie 2 Guido toevoegen deling in ontwikkeling

--- src/main/java/Rekenen.java
+++ src/main/java/Rekenen.java
@@ -14,7 +14,9 @@ public class Rekenen {
     public int vermenigvuldigen (int getal1, int getal2) {
         return getal1 * getal2;
     }
+
     public int delen (int getal1, int getal2) {
-        return 0;   // in ontwikkeling
+        return 0;  // nog in ontwikkeling
     }
+
 }
